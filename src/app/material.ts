eced778d85f72f22794008dd6341d1e138b8d5ff
James Lyons <lyons-j@kcusd.com>
2019-04-12 08:59:41 -0700

    began user input form

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,10 +1,16 @@
-import { MatButtonModule, MatCheckboxModule, MatCardModule, MatProgressSpinnerModule, MatProgressBarModule } from '@angular/material';
+import { MatButtonModule, 
+  MatCheckboxModule, 
+  MatProgressSpinnerModule, 
+  MatProgressBarModule 
+} from '@angular/material';
+import {MatCardModule} from '@angular/material/card'
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
 import { NgModule } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatProgressSpinnerModule, MatMenuModule, MatIconModule, MatProgressSpinnerModule, MatProgressBarModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatProgressSpinnerModule, MatMenuModule, MatIconModule, MatProgressSpinnerModule, MatProgressBarModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatProgressSpinnerModule, MatMenuModule, MatIconModule, MatProgressSpinnerModule, MatProgressBarModule, MatCardModule, MatSelectModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatCardModule, MatProgressSpinnerModule, MatMenuModule, MatIconModule, MatProgressSpinnerModule, MatProgressBarModule, MatCardModule, MatSelectModule],
 })
 export class MaterialModule { }
